6384310fd76589faa8a4da0b89cbacd393042782
eddiejpot <jamesedwardporter@gmail.com>
2021-08-16 17:50:54 +0800

    added tablecomponent for alltrips

---
+++ src/components/Tables/TripTable.jsx
@@ -0,0 +1,86 @@
+/* =================================================================== */
+/* =========================================================== IMPORTS */
+/* =================================================================== */
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+/* =================================================================== */
+/* ================================================ CONTEXT / REDUCERS */
+/* =================================================================== */
+import { AllTripsContext, loadAllTrips } from '../../Context/UserAllTrips.jsx';
+
+/* =================================================================== */
+/* ============================================================== MAIN */
+/* =================================================================== */
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+export default function TripTable() {
+  const history = useHistory();
+  const classes = useStyles();
+
+  // State Management
+  // initialize the state from the context provider
+  const { state, dispatch } = useContext(AllTripsContext);
+  // // On page load
+  useEffect(() => {
+    // get all trips data
+    loadAllTrips(dispatch);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const ComponentToRender = () => {
+    console.log('TRIPTABLE STATE');
+    console.log(state);
+
+    for (let i = 0; i < state.trips.length; i += 1) {
+      console.log(state.trips[i].name);
+      console.log(state.trips[i].numOfRoutes);
+    }
+
+    if (state.trips.length === 0) {
+      return <h1>loading...</h1>;
+    }
+    return (
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>TRIP</TableCell>
+              <TableCell align="left">NO. OF ROUTES</TableCell>
+              <TableCell align="left">FROM</TableCell>
+              <TableCell align="left">TO</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {state.trips.map((row) => (
+              <TableRow key={row.id} onClick={() => history.push(`/trip/${row.id}`)}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="left">{row.numOfRoutes}</TableCell>
+                <TableCell align="left">{row.startDate}</TableCell>
+                <TableCell align="left">{row.endDate}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
+
+  return (
+    <ComponentToRender />
+  );
+}
--- src/components/Trip/AllTrips.jsx
+++ src/components/Trip/AllTrips.jsx
@@ -1,14 +1,17 @@
 /* =================================================================== */
 /* =========================================================== IMPORTS */
 /* =================================================================== */
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 // eslint-disable-next-line import/no-cycle
 import { useHistory } from 'react-router-dom';
+import TripTable from '../Tables/TripTable.jsx';
+import AllTripsNavBar from '../NavBar/AllTripsNavBar.jsx';
 
 /* =================================================================== */
-/* =========================================================== CONTEXT */
+/* ================================================ CONTEXT / REDUCERS */
 /* =================================================================== */
-import { isLoggedInContext } from '../../Context/isLoggedIn.jsx';
+import { isLoggedInContext } from '../../Context/IsLoggedIn.jsx';
+import { AllTripProvider } from '../../Context/UserAllTrips.jsx';
 
 /* =================================================================== */
 /* ============================================================== MAIN */
@@ -20,18 +23,26 @@ export default function AllTrips() {
   if (!isLoggedIn) {
     history.push('/');
   }
-  // State Management
+  // // State Management
+  // // initialize the state from the context provider
+  // const { state, dispatch } = useContext(AllTripsContext);
+  // // This page should reload only when a new route is added or details of a trip are changed
 
-  // On page load
-  useEffect(() => {
-
-  }, []);
+  // // On page load
+  // useEffect(() => {
+  //   // get all trips data
+  //   loadAllTrips(dispatch);
+  // }, []);
+  console.log('ALL TRIPS PAGE');
 
   /* =========================================================== RENDER */
   return (
     <>
+      <AllTripsNavBar />
       <h1>ALL TRIPS</h1>
-      <p>To do:  Trips Table Component</p>
+      <AllTripProvider>
+        <TripTable />
+      </AllTripProvider>
     </>
   );
 }
